57da6e915adf5f6517649a4bc00e9238465861e5
ShoweryCellar34 <126352167+ShoweryCellar34@users.noreply.github.com>
2026-02-16 14:40:16 +1300

    texture.h basic data structure + glfw bump

--- headers/SBX/texture.h
+++ headers/SBX/texture.h
@@ -2,11 +2,19 @@
 #define SBX_TEXTURE_H
 
 // Project headers
+#include <SBX/types.h>
 
 // Dependency headers
+#include <PR/texture.h>
 
 /// @brief Structure used by SBXTexture fuctions to store texture handle, associated SBXWindow, and more data to represent a texture
 struct SBXTexture {
+    // State keeping
+    SBX_bool_t     initialized;
+
+    // Handles
+    SBX_window_t*     associatedWindow;
+    prTextureData* textureHandle;
 };
 
 #endif // SBX_TEXTURE_H
